f245d28250510b61afa910b9b5ef94766ffdecd6
istvantothdesign <69763751+istvantothdesign@users.noreply.github.com>
2022-01-28 18:26:35 +0000

    My beatmaker added

---
+++ My Beatmaker/beatmaker.js
@@ -0,0 +1,23 @@
+class DrumKit {
+  constructor() {
+    this.pads = document.querySelectorAll(".pad");
+    this.kickAudio = document.querySelector(".kick-sound");
+    this.snareAudio = document.querySelector(".snare-sound");
+    this.hihatAudio = document.querySelector(".hihat-sound");
+    this.index = 0;
+  }
+  repeat() {
+    let step = this.index % 8;
+    console.log(this.index);
+    this.index++;
+  }
+  start() {
+    setInterval(() => {
+      this.repeat();
+    }, 1000);
+  }
+}
+
+const drumKit = new DrumKit();
+
+drumKit.start();
