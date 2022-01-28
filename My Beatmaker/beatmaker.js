1ff7ee2c3bfd37f88d1f51c710633f7b1accd6e7
Istvan <istvan.toth@astutis.com>
2022-01-28 19:23:55 +0000

    Beatmaker play btn

--- My Beatmaker/beatmaker.js
+++ My Beatmaker/beatmaker.js
@@ -1,23 +1,29 @@
 class DrumKit {
   constructor() {
     this.pads = document.querySelectorAll(".pad");
+    this.playBtn = document.querySelector(".play");
     this.kickAudio = document.querySelector(".kick-sound");
     this.snareAudio = document.querySelector(".snare-sound");
     this.hihatAudio = document.querySelector(".hihat-sound");
     this.index = 0;
+    this.bpm = document.querySelector(".tempo-slider").value;
   }
   repeat() {
     let step = this.index % 8;
-    console.log(this.index);
+    const activeBars = document.querySelectorAll(`.b${step}`);
+    console.log(activeBars);
     this.index++;
   }
   start() {
+    const interval = (60 / this.bpm) * 1000;
     setInterval(() => {
       this.repeat();
-    }, 1000);
+    }, interval);
   }
 }
 
 const drumKit = new DrumKit();
 
-drumKit.start();
+drumKit.playBtn.addEventListener("click", function () {
+  drumKit.start();
+});
